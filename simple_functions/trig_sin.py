d6c55f876748eefd784ab77d0a62542ec09b7389
acse-cxd20 <calvinxdavies@icloud.com>
2020-10-21 12:28:49 +0100

    Adding sin function

--- simple_functions/trig_sin.py
+++ simple_functions/trig_sin.py
@@ -1,11 +1,11 @@
-from numpy import sqrt
 from functools import lru_cache
 from simple_functions.functions1 import factorial
 
 __all__ = ['my_sin']
 
+
 @lru_cache(maxsize=None)  # Note: -> @cache in python >= 3.9
-def my_sin(x,n):
+def my_sin(x, n):
     sine = 0
     for i in range(n):
         sine += ((-1)**i)*(x**(2.*i + 1))/(factorial(2.*i + 1.))
--- tests/test_trig_sin.py
+++ tests/test_trig_sin.py
@@ -2,10 +2,12 @@ import numpy as np
 
 from simple_functions import my_sin
 
+
 class TestSin(object):
     ''' Class to test if sin approximation is working as intended'''
 
     def test_sin(self):
         x = 2*np.pi
-        sin_approx = my_sin(x,100)
+        sin_approx = my_sin(x, 100)
         assert np.isclose(sin_approx, np.sin(x), atol=1.0e-12)
+        
